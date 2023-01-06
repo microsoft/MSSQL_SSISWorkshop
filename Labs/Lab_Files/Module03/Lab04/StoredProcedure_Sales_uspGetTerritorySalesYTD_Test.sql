baaa87fe70422c8f4552f849e200d8145a246a52
Mohit K. Gupta <mohitkgupta@msn.com>
2023-01-06 14:24:47 -0700

    v2023
    Updated PowerPoints, Labs, and Files.

--- Labs/Lab_Files/Module03/Lab04/StoredProcedure_Sales_uspGetTerritorySalesYTD_Test.sql
+++
@@ -1,9 +0,0 @@
-USE AdventureWorks2012
-GO
-
---testing sproc execution
-DECLARE @SalesYTD REAL 
-EXEC Sales.uspGetTerritorySalesYTD   @CountryRegionCode='fr' /*US,CA,FR,DE,AU,GB*/
-								   , @SalesYTD=@SalesYTD OUTPUT
-SELECT @SalesYTD AS SalesYTD
-GO
